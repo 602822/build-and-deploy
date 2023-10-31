a81af3ebe959ab0f7e09908879942764d260967e
602822 <89077126+602822@users.noreply.github.com>
2023-10-31 19:26:19 +0100

    added minutes left of year

--- code/src/clock.js
+++ code/src/clock.js
@@ -1,19 +1,30 @@
 export const getTimeString = (now) => {
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const seconds = now.getSeconds();
-    return `${hour}:${minute}:${seconds}`
-}
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const seconds = now.getSeconds();
+  return `${hour}:${minute}:${seconds}`;
+};
 
 const getNextYearDate = (now) => {
-    const endOfYearDate = new Date(0)
-    endOfYearDate.setUTCFullYear(now.getUTCFullYear() + 1)
-    return endOfYearDate;
-}
+  const endOfYearDate = new Date(0);
+  endOfYearDate.setUTCFullYear(now.getUTCFullYear() + 1);
+  return endOfYearDate;
+};
 
 export const getSecondsLeftOfYear = (now) => {
-    const startOfNextYear = getNextYearDate(now);
-    const startOfNextYearMillis = startOfNextYear.getTime();
-    const nowMillis = now.getTime();
-    return Math.floor((startOfNextYearMillis - nowMillis) / 1000);
-}
+  const startOfNextYear = getNextYearDate(now);
+  const startOfNextYearMillis = startOfNextYear.getTime();
+  const nowMillis = now.getTime();
+  return Math.floor((startOfNextYearMillis - nowMillis) / 1000);
+};
+
+export const getMinutesLeftOfYear = (now) => {
+  const currentDate = new Date();
+  const endOfYearDate = getNextYearDate(Date.now);
+
+  const timeDifference = endOfYearDate - currentDate;
+
+  const minutesLeft = (timeDifference / 60000).toFixed(2);
+
+  return minutesLeft;
+};
--- code/src/main.js
+++ code/src/main.js
@@ -1,12 +1,18 @@
-import { getSecondsLeftOfYear, getTimeString } from "./clock";
+import {
+  getMinutesLeftOfYear,
+  getSecondsLeftOfYear,
+  getTimeString,
+} from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const minutesLeft = document.getElementById("minutes-left");
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
   timeContainer.innerText = getTimeString(now);
+  minutesLeft.innerHTML = getMinutesLeftOfYear(now);
 };
 
 render();
